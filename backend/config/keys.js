0d661332b50698a90e638531c93db3bd024c1c89
farivar <farivaramiri@gmail.com>
2025-01-05 23:10:16 -0800

    fix the port

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -4,8 +4,3 @@ module.exports = {
   isProduction: process.env.NODE_ENV === 'production'
 };
   
-console.log('Environment Variables:', {
-  MONGO_URI: process.env.MONGO_URI || 'Not Set',
-  SECRET_OR_KEY: process.env.SECRET_OR_KEY || 'Not Set',
-  NODE_ENV: process.env.NODE_ENV || 'Not Set',
-});
